2062a79973f69864649aab51b2d2ab672489ce28
fdm1 <frankmassi@gmail.com>
2017-01-21 21:37:56 -0600

    year 2015 - day 2 - part 2

--- code_advent/year_2015/day_02.py
+++ code_advent/year_2015/day_02.py
@@ -1,9 +1,14 @@
 """ http://adventofcode.com/2015/day/2 """
 
 
+def box_lengths(dimensions):
+    """Return list of dimension ints"""
+    return sorted([int(i) for i in dimensions.split('x')])
+
+
 def get_sides(dimensions):
     """Return sorted list of side areas"""
-    lengths = [int(i) for i in dimensions.split('x')]
+    lengths = box_lengths(dimensions)
     return sorted(lengths[i]*lengths[i-1] for i in range(3))
 
 
@@ -14,4 +19,29 @@ def get_box_paper(dimensions):
 
 
 def get_total_paper(boxes):
+    """Get all paper needed for a list of boxes"""
     return sum(get_box_paper(b) for b in boxes)
+
+
+def get_smallest_side_perimeter(dimensions):
+    """Return the smallest perimeter of a box"""
+    lengths = box_lengths(dimensions)
+    return lengths[0]*2 + lengths[1]*2
+
+
+def get_cubic_feet(dimensions):
+    """Return cubic feet of a box"""
+    res = 1
+    for i in box_lengths(dimensions):
+        res *= i
+    return res
+
+
+def get_box_ribbon(dimensions):
+    """Return total ribbon needed for a box"""
+    return get_smallest_side_perimeter(dimensions) + get_cubic_feet(dimensions)
+
+
+def get_total_ribbon(boxes):
+    """Return ribbon needed for list of boxes"""
+    return sum(get_box_ribbon(b) for b in boxes)
--- tests/test_year_2015/test_day_02.py
+++ tests/test_year_2015/test_day_02.py
@@ -21,3 +21,32 @@ def test_get_box_paper(dimensions, box_paper):
 def test_get_total_paper():
     boxes = ['2x3x4', '1x1x10']
     assert day_02.get_total_paper(boxes) == 101
+
+
+@pytest.mark.parametrize('dimensions, perimeter', (
+    ('2x3x4', 10),
+    ('1x1x10', 4),
+    ))
+def test_get_smallest_side_perimeter(dimensions, perimeter):
+    assert day_02.get_smallest_side_perimeter(dimensions) == perimeter
+
+
+@pytest.mark.parametrize('dimensions, cubic_feet', (
+    ('2x3x4', 24),
+    ('1x1x10', 10),
+    ))
+def test_get_cubic_feet(dimensions, cubic_feet):
+    assert day_02.get_cubic_feet(dimensions) == cubic_feet
+
+
+@pytest.mark.parametrize('dimensions, ribbon_length', (
+    ('2x3x4', 34),
+    ('1x1x10', 14),
+    ))
+def test_get_box_ribbon(dimensions, ribbon_length):
+    assert day_02.get_box_ribbon(dimensions) == ribbon_length
+
+
+def test_get_total_ribbon():
+    boxes = ['2x3x4', '1x1x10']
+    assert day_02.get_total_ribbon(boxes) == 48
