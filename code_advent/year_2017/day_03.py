e1fb2829a29a4e30709453a796b4db97de2ec9c0
Frank Massi <frankmassi@gmail.com>
2017-12-04 17:26:37 -0600

    2017 day 3 part 1

---
+++ code_advent/year_2017/day_03.py
@@ -0,0 +1,30 @@
+"""2017 day 3 http://adventofcode.com/2017/day/3"""
+import sys
+
+
+def ring_level(num):
+    """Return the side lenth, level, and max value of a spiral ring of consecutive ints"""
+    max_value = 1
+    level = 0
+    side_len = 1
+    while max_value < num:
+        level = level + 1
+        side_len = (level * 2) + 1
+        max_value = side_len + side_len * (side_len - 1)
+    return side_len, level, max_value
+
+
+def get_distance(num):
+    """Return the Manhattan Distance for a number in a spiral of consecutive ints"""
+
+    side_len, level, max_value = ring_level(num)
+    while max_value > num:
+        max_value = max_value - (side_len - 1)
+    lateral = abs(num - (max_value + int(side_len/2)))
+    inwards = level
+    return lateral + inwards
+
+
+if __name__ == '__main__':
+
+    print(get_distance(sys.argv[1]))
---
+++ tests/year_2017/test_2017_03.py
@@ -0,0 +1,21 @@
+import pytest
+
+from code_advent.year_2017 import day_03
+
+
+@pytest.mark.parametrize('num,result', (
+    (4, (3, 1, 9)),
+    (10, (5, 2, 25)),
+    (50, (9, 4, 81)),
+))
+def test_ring_level(num, result):
+    assert day_03.ring_level(num) == result
+
+
+@pytest.mark.parametrize('num,result', (
+    (4, 1),
+    (10, 3),
+    (50, 7),
+))
+def test_get_distance(num, result):
+    assert day_03.get_distance(num) == result
