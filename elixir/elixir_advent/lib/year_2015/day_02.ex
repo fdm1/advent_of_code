6c396dccc05b3679dcd5b57121c9c286593cf52b
Frank Massi <frankmassi@gmail.com>
2018-11-14 08:52:32 -0600

    Elixir - Refactor 2015 day 1 part 1 to prep for part 2

--- elixir/elixir_advent/lib/year_2015/day_02.ex
+++ elixir/elixir_advent/lib/year_2015/day_02.ex
@@ -1,17 +1,24 @@
 # 2015 - Day 02: http://adventofcode.com/2015/day/2
-
 defmodule ElixirAdvent.Year2015.Day02 do
   def part1(input) do
-    dim_strings = String.split(ElixirAdvent.read_input(input), "\n")
-    Enum.sum(Enum.map(dim_strings, fn(dim) -> paper_for_box(dim) end))
+    Enum.sum(Enum.map(input_to_dim_list(input), fn(dim) -> paper_for_box(dim) end))
   end
 
   def part2(input) do
-    ElixirAdvent.read_input(input)
+    part1(input)
+    # ElixirAdvent.read_input(input)
   end
 
-  def paper_for_box(dimension_string) do
-    dim = Enum.map(String.split(dimension_string, "x"), fn(i) -> String.to_integer(i) end)
+  def input_to_dim_list(input) do
+    dim_strings = String.split(input, "\n")
+    Enum.map(dim_strings, fn(s) ->
+      Enum.map(String.split(s, "x"), fn(i) ->
+        String.to_integer(i)
+      end)
+    end)
+  end
+
+  def paper_for_box(dim) do
     sides = Enum.map(
       0..length(dim)-1,
       fn i -> Enum.at(dim, i) * Enum.at(dim, i-1) end
--- elixir/elixir_advent/test/test_year_2015/day_02_test.exs
+++ elixir/elixir_advent/test/test_year_2015/day_02_test.exs
@@ -9,13 +9,24 @@ defmodule ElixirAdvent.Year2015.Day02Test do
     ]
   end
 
+  def input_string do
+    Enum.join(Enum.map(test_cases(), fn(case) -> Enum.at(case, 0) end), "\n")
+  end
+
+  test "input_to_dim_list" do
+    assert ElixirAdvent.Year2015.Day02.input_to_dim_list(input_string()) == [[2, 3, 4], [1, 1, 10]]
+  end
+
+
   test "paper_for_box" do
-    for n <- test_cases, do: assert ElixirAdvent.Year2015.Day02.paper_for_box(Enum.at(n, 0)) == Enum.at(n, 1)
+    Enum.map(test_cases(), fn(n) ->
+      dim = Enum.map(String.split(Enum.at(n, 0), "x"), fn(i) -> String.to_integer(i) end)
+      assert ElixirAdvent.Year2015.Day02.paper_for_box(dim) == Enum.at(n, 1)
+    end)
   end
 
   test "part1" do
-    input = Enum.join(Enum.map(test_cases, fn(case) -> Enum.at(case, 0) end), "\n")
-    total_sum = Enum.sum(Enum.map(test_cases, fn(case) -> Enum.at(case, 1) end))
-    assert ElixirAdvent.Year2015.Day02.part1(input) == total_sum
+    total_sum = Enum.sum(Enum.map(test_cases(), fn(case) -> Enum.at(case, 1) end))
+    assert ElixirAdvent.Year2015.Day02.part1(input_string()) == total_sum
   end
 end
