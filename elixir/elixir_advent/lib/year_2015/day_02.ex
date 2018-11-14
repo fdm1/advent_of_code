f7a310589fbb99ca9a5d636e801420920a982ad0
Frank Massi <frankmassi@gmail.com>
2018-11-14 08:09:58 -0600

    2015 day 2 part 1 in elixir

--- elixir/elixir_advent/lib/year_2015/day_02.ex
+++ elixir/elixir_advent/lib/year_2015/day_02.ex
@@ -1,11 +1,23 @@
-# 2015 - Day DD: http://adventofcode.com/2015/day/2
+# 2015 - Day 02: http://adventofcode.com/2015/day/2
 
 defmodule ElixirAdvent.Year2015.Day02 do
   def part1(input) do
-    ElixirAdvent.read_input(input)
+    dim_strings = String.split(ElixirAdvent.read_input(input), "\n")
+    Enum.sum(Enum.map(dim_strings, fn(dim) -> paper_for_box(dim) end))
   end
 
   def part2(input) do
     ElixirAdvent.read_input(input)
+  end
+
+  def paper_for_box(dimension_string) do
+    dim = Enum.map(String.split(dimension_string, "x"), fn(i) -> String.to_integer(i) end)
+    sides = Enum.map(
+      0..length(dim)-1,
+      fn i -> Enum.at(dim, i) * Enum.at(dim, i-1) end
+    )
+    min_side = Enum.min(sides)
+
+    Enum.sum(Enum.map(sides, fn(side) -> 2 * side end)) + min_side
   end
 end
--- elixir/elixir_advent/test/test_year_2015/day_02_test.exs
+++ elixir/elixir_advent/test/test_year_2015/day_02_test.exs
@@ -1,4 +1,21 @@
 defmodule ElixirAdvent.Year2015.Day02Test do
   use ExUnit.Case
   doctest ElixirAdvent.Year2015.Day02
+
+  def test_cases do
+    [
+      ["2x3x4", 58],
+      ["1x1x10", 43]
+    ]
+  end
+
+  test "paper_for_box" do
+    for n <- test_cases, do: assert ElixirAdvent.Year2015.Day02.paper_for_box(Enum.at(n, 0)) == Enum.at(n, 1)
+  end
+
+  test "part1" do
+    input = Enum.join(Enum.map(test_cases, fn(case) -> Enum.at(case, 0) end), "\n")
+    total_sum = Enum.sum(Enum.map(test_cases, fn(case) -> Enum.at(case, 1) end))
+    assert ElixirAdvent.Year2015.Day02.part1(input) == total_sum
+  end
 end
