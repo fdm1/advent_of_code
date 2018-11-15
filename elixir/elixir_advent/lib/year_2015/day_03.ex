9d2247c4fd3fd8cbf3a7002a239ee1a55e0f0b28
Frank Massi <frankmassi@gmail.com>
2018-11-15 08:22:17 -0600

    elixir - add 2015 day 3 scaffolding/input

---
+++ elixir/elixir_advent/lib/year_2015/day_03.ex
@@ -0,0 +1,11 @@
+# 2015 - Day 03: http://adventofcode.com/2015/day/3
+
+defmodule ElixirAdvent.Year2015.Day03 do
+  def part1(input) do
+    ElixirAdvent.read_input(input)
+  end
+
+  def part2(input) do
+    ElixirAdvent.read_input(input)
+  end
+end
---
+++ elixir/elixir_advent/test/test_year_2015/day_03_test.exs
@@ -0,0 +1,4 @@
+defmodule ElixirAdvent.Year2015.Day03Test do
+  use ExUnit.Case
+  doctest ElixirAdvent.Year2015.Day03
+end
