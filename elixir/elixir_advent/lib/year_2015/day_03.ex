cda25d1ffd37a66c3329852b6b2a87a98cfe31b7
Frank Massi <frankmassi@gmail.com>
2018-12-03 07:51:48 -0600

    elixir - 2015-3-1 not working yet

--- elixir/elixir_advent/lib/year_2015/day_03.ex
+++ elixir/elixir_advent/lib/year_2015/day_03.ex
@@ -2,10 +2,37 @@
 
 defmodule ElixirAdvent.Year2015.Day03 do
   def part1(input) do
-    ElixirAdvent.read_input(input)
+    directions = directions_to_list(input)
+
+    current_position = [0,0]
+    houses = MapSet.new([current_position])
+
+    for d <- directions do
+      current_position = move(current_position, d)
+      houses = MapSet.put(houses, current_position)
+      IO.inspect(houses)
+      IO.inspect(current_position)
+    end
+
+    IO.inspect(houses)
+    IO.puts("current size is #{MapSet.size(houses)}")
+    MapSet.size(houses)
   end
 
   def part2(input) do
     ElixirAdvent.read_input(input)
+  end
+
+  def move(position, direction) do
+    case (direction) do
+      ">" -> [Enum.at(position, 0) + 1, Enum.at(position, 1)]
+      "<" -> [Enum.at(position, 0) - 1, Enum.at(position, 1)]
+      "^" -> [Enum.at(position, 0), Enum.at(position, 1) + 1]
+      "v" -> [Enum.at(position, 0), Enum.at(position, 1) - 1]
+    end
+  end
+
+  def directions_to_list(input) do
+    List.delete(String.split(input, "", trim: True), "")
   end
 end
--- elixir/elixir_advent/test/test_year_2015/day_03_test.exs
+++ elixir/elixir_advent/test/test_year_2015/day_03_test.exs
@@ -1,4 +1,12 @@
 defmodule ElixirAdvent.Year2015.Day03Test do
   use ExUnit.Case
   doctest ElixirAdvent.Year2015.Day03
+
+  test "directions_to_list" do
+    assert ElixirAdvent.Year2015.Day03.directions_to_list("<<^^>>vv") == ["<", "<", "^", "^", ">", ">", "v", "v"]
+  end
+
+  test "part1" do
+    assert ElixirAdvent.Year2015.Day03.part1(">") == 2
+  end
 end
