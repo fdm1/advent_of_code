8e7fe0944a1625613788d7373e46ce14cce513ad
Frank Massi <frankmassi@gmail.com>
2019-11-01 13:40:30 -0500

    wip

--- elixir/lib/year_2018/day_03.ex
+++ elixir/lib/year_2018/day_03.ex
@@ -2,7 +2,44 @@
 
 defmodule ElixirAdvent.Year2018.Day03 do
   def part1(input) do
-    ElixirAdvent.read_input(input)
+    rects = Enum.map(Enum.map(String.split(ElixirAdvent.read_input(input), "\n"), &parse_rect/1), &rect_points/1)
+
+    all_int = Enum.reduce(rects, [Enum.drop(rects, 1), []], fn rect, acc ->
+      new_rects = Enum.at(acc, 0)
+      all_intersects = Enum.at(acc, 1)
+      new_intersects = MapSet.new()
+      new_intersects = for r <- new_rects do
+        MapSet.union(new_intersects, intersect(rect, r))
+      end
+      [Enum.drop(new_rects, 1), MapSet.union(all_intersects, new_intersects)]
+    end)
+
+    length(Enum.at(all_int, 1))
+
+  end
+
+  def parse_rect(input_str) do
+    Enum.drop(
+      Enum.map(
+        Enum.filter(
+          String.split(input_str, ""), fn i -> Regex.match?(~r/[0-9]/, i) end),
+        fn i -> String.to_integer(i) end
+      ),
+    1)
+  end
+
+  def rect_points(rect_specs) do
+    res = MapSet.new()
+    for h <- Range.new(0, Enum.at(rect_specs, 2)-1) do
+      for w <- Range.new(0, Enum.at(rect_specs, 3)-1) do
+        MapSet.put(res, [Enum.at(rect_specs, 0) + h, Enum.at(rect_specs, 1) + w])
+      end
+    end
+#    Enum.reduce(List.flatten(res), fn i, acc -> MapSet.union(i, acc) end)
+  end
+
+  def intersect(points1, points2) do
+    MapSet.intersection(points1, points2)
   end
 
   def part2(input) do
--- elixir/test/test_year_2018/day_03_test.exs
+++ elixir/test/test_year_2018/day_03_test.exs
@@ -3,9 +3,43 @@ defmodule ElixirAdvent.Year2018.Day03Test do
   doctest ElixirAdvent.Year2018.Day03
 
   test "part1" do
-    input = ""
-    answer = ""
-    assert ElixirAdvent.Year2018.Day03.part1(input) == ""
+    input = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"
+    assert ElixirAdvent.Year2018.Day03.part1(input) == 4
+  end
+
+  test "parse_rect" do
+    assert ElixirAdvent.Year2018.Day03.parse_rect("#1 @ 1,3: 4x4") == [1, 3, 4, 4]
+  end
+
+  test "rect_points" do
+    assert ElixirAdvent.Year2018.Day03.rect_points([1,3, 4, 4]) == MapSet.new([
+      [1,3], [1, 4], [1, 5], [1, 6],
+      [2,3], [2, 4], [2, 5], [2, 6],
+      [3,3], [3, 4], [3, 5], [3, 6],
+      [4,3], [4, 4], [4, 5], [4, 6]
+    ])
+  end
+
+  test "intersect" do
+    rect1 = MapSet.new([
+      [1,3], [1, 4], [1, 5], [1, 6],
+      [2,3], [2, 4], [2, 5], [2, 6],
+      [3,3], [3, 4], [3, 5], [3, 6],
+      [4,3], [4, 4], [4, 5], [4, 6]
+    ])
+
+    rect2 = MapSet.new([
+      [3, 1], [3, 2], [3, 3], [3, 4],
+      [4, 1], [4, 2], [4, 3], [4, 4],
+      [5, 1], [5, 2], [5, 3], [5, 4],
+      [6, 1], [6, 2], [6, 3], [6, 4]
+    ])
+
+    intersection = MapSet.new([
+      [3,3], [3, 4],
+      [4,3], [4, 4]
+    ])
+    assert ElixirAdvent.Year2018.Day03.intersect(rect1, rect2) == intersection
   end
 
   test "part2" do
