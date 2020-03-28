5ffb15e682179f43b5d35fec6f136823eb4a8fd1
Frank Massi <frankmassi@gmail.com>
2020-03-28 14:23:17 -0500

    2019 day 1 part 2 in TS

--- ts_aoc/src/challenges/year-2019/day-1.ts
+++ ts_aoc/src/challenges/year-2019/day-1.ts
@@ -5,6 +5,16 @@ export const getFuel = (mass: number): number => {
   return Math.floor(mass / 3) - 2;
 };
 
+export const getFuelRecursive = (mass: number): number => {
+  let currentMassFuel = getFuel(mass);
+  let totalFuel = 0;
+  while (currentMassFuel > 0) {
+    totalFuel += currentMassFuel;
+    currentMassFuel = getFuel(currentMassFuel);
+  }
+  return totalFuel;
+};
+
 const input: string = getInput(2019, 1);
 
 const part1 = (): number => {
@@ -19,7 +29,19 @@ const part1 = (): number => {
     .reduce((sum: number, current: number) => sum + current, 0);
 };
 
+const part2 = (): number => {
+  return input
+    .split('\n')
+    .map((n: string) => {
+      return parseInt(n);
+    })
+    .map((mass: number) => {
+      return getFuelRecursive(mass);
+    })
+    .reduce((sum: number, current: number) => sum + current, 0);
+};
+
 export const year2019day1: ChallengeResultFunctions = {
   part1: part1,
-  part2: undefined,
+  part2: part2,
 };
--- ts_aoc/test/challenges/year-2019/day-1-test.ts
+++ ts_aoc/test/challenges/year-2019/day-1-test.ts
@@ -1,15 +1,21 @@
 import { NumberInNumberOut } from '@test/test-types';
-import { getFuel } from '../../../src/challenges/year-2019/day-1';
+import { getFuel, getFuelRecursive } from '../../../src/challenges/year-2019/day-1';
 
 const testCases: NumberInNumberOut[] = [
-  { input: 12, part1Solution: 2 },
-  { input: 14, part1Solution: 2 },
-  { input: 1969, part1Solution: 654 },
-  { input: 100756, part1Solution: 33583 },
+  { input: 12, part1Solution: 2, part2Solution: 2 },
+  { input: 14, part1Solution: 2, part2Solution: 2 },
+  { input: 1969, part1Solution: 654, part2Solution: 966 },
+  { input: 100756, part1Solution: 33583, part2Solution: 50346 },
 ];
 
 test('getFuel works', () => {
   testCases.map((testCase: NumberInNumberOut) => {
     expect(getFuel(testCase.input)).toEqual(testCase.part1Solution);
+  });
+});
+
+test('getFuelRecursive works', () => {
+  testCases.map((testCase: NumberInNumberOut) => {
+    expect(getFuelRecursive(testCase.input)).toEqual(testCase.part2Solution);
   });
 });
