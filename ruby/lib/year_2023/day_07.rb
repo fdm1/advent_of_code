8f5941f98ed4bf13fadf890953290060d30bd34f
Frank Massi <fdm1@users.noreply.github.com>
2023-12-07 11:44:18 -0600

    2023 day 7 - ruby (#23)
    * WIP: setup 2023-7
    
    * WIP: 250609983 too high part 1
    
    * part 1
    
    * WIP: part 2 - 250952490 - too high
    
    * part 2

---
+++ ruby/lib/year_2023/day_07.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+module Year2023
+  class Day07 < AdventOfCode::PuzzleBase
+    def part1
+      determine_value
+    end
+
+    def part2
+      @hands.map do |hand|
+        hand[:type] = use_jokers(hand[:hand])
+      end
+      sort_hands({ 11 => 1 })
+      determine_value
+    end
+
+    def determine_value
+      value = 0
+      @hands.each_with_index do |hand, index|
+        value += hand[:bid] * (index + 1)
+      end
+      value
+    end
+
+    HAND_TYPES = %i[
+      five_of_a_kind
+      four_of_a_kind
+      full_house
+      three_of_a_kind
+      two_pair
+      one_pair
+      high_card
+    ].freeze
+
+    def card_value(card)
+      case card
+      when 'A'
+        14
+      when 'K'
+        13
+      when 'Q'
+        12
+      when 'J'
+        11
+      when 'T'
+        10
+      else
+        card.to_i
+      end
+    end
+
+    def use_jokers(hand)
+      jokers = hand.select { |card| card == 11 }
+      new_hand = hand.reject { |card| card == 11 }
+      most_common_card = new_hand.max_by { |card| hand.count(card) }
+      highest_card = new_hand.max
+      jokers.count.times do
+        new_hand << if hand.count(most_common_card) == 1
+                      highest_card
+                    else
+                      most_common_card
+                    end
+      end
+      determine_hand_value(new_hand)
+    end
+
+    def determine_hand_value(hand)
+      uniq_length = hand.uniq.length
+      case uniq_length
+      when 5
+        :high_card
+      when 4
+        :one_pair
+      when 3
+        if hand.any? { |card| hand.count(card) == 3 }
+          :three_of_a_kind
+        else
+          :two_pair
+        end
+      when 2
+        if hand.any? { |card| hand.count(card) == 4 }
+          :four_of_a_kind
+        else
+          :full_house
+        end
+      when 1
+        :five_of_a_kind
+      end
+    end
+
+    def sort_hands(value_overrides = {})
+      sorted_hands = []
+      HAND_TYPES.each do |hand_type|
+        type_hands = @hands.select { |hand| hand[:type] == hand_type }
+        sorted_hands << type_hands.sort_by do |hand|
+          hand[:hand].map do |h|
+            value_overrides[h] || h
+          end.map(&:to_s).map { |n| n.rjust(2, '0') }.join # rubocop:disable Style/MultilineBlockChain
+        end.reverse
+      end
+      @hands = sorted_hands.flatten.reverse
+    end
+
+    # setup gets called as part of initialize
+    def setup
+      @hands = []
+      @input.split("\n").collect do |player|
+        hand, bid = player.split
+        hand = hand.chars.map { |card| card_value(card) }
+        bid = bid.to_i
+        @hands << { hand:, bid:, type: determine_hand_value(hand) }
+      end
+      sort_hands
+    end
+  end
+end
