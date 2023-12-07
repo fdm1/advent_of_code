8ce32ef5b1ae2bdaac450df8dcceef9f9b027925
Frank Massi <frankmassi@gmail.com>
2023-12-07 11:26:21 -0600

    part 1

--- ruby/lib/year_2023/day_07.rb
+++ ruby/lib/year_2023/day_07.rb
@@ -12,15 +12,15 @@ module Year2023
 
     def part2; end
 
-    HAND_TYPES = {
-      five_of_a_kind: 6,
-      four_of_a_kind: 5,
-      full_house: 4,
-      three_of_a_kind: 3,
-      two_pair: 2,
-      one_pair: 1,
-      high_card: 0
-    }.freeze
+    HAND_TYPES = %i[
+      five_of_a_kind
+      four_of_a_kind
+      full_house
+      three_of_a_kind
+      two_pair
+      one_pair
+      high_card
+    ].freeze
 
     def card_value(card)
       case card
@@ -43,31 +43,31 @@ module Year2023
       uniq_length = hand.uniq.length
       case uniq_length
       when 5
-        HAND_TYPES[:high_card]
+        :high_card
       when 4
-        HAND_TYPES[:one_pair]
+        :one_pair
       when 3
-        if hand.count(hand[0]) == 3 || hand.count(hand[1]) == 3 || hand.count(hand[2]) == 3
-          HAND_TYPES[:three_of_a_kind]
+        if hand.any? { |card| hand.count(card) == 3 }
+          :three_of_a_kind
         else
-          HAND_TYPES[:two_pair]
+          :two_pair
         end
       when 2
-        if hand.count(hand[0]) == 1 || hand.count(hand[0]) == 4
-          HAND_TYPES[:four_of_a_kind]
+        if hand.any? { |card| hand.count(card) == 4 }
+          :four_of_a_kind
         else
-          HAND_TYPES[:full_house]
+          :full_house
         end
       when 1
-        HAND_TYPES[:five_of_a_kind]
+        :five_of_a_kind
       end
     end
 
     def sort_hands
       sorted_hands = []
-      HAND_TYPES.each_value do |hand_type|
+      HAND_TYPES.each do |hand_type|
         type_hands = @hands.select { |hand| hand[:type] == hand_type }
-        sorted_hands << type_hands.sort_by { |hand| hand[:hand].map(&:to_s).join }.reverse
+        sorted_hands << type_hands.sort_by { |hand| hand[:hand].map(&:to_s).map { |n| n.rjust(2, '0') }.join }.reverse
       end
       @hands = sorted_hands.flatten.reverse
     end
