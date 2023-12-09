4dce4bac22df6e0f19864c521a76c8ad504187c4
Frank Massi <frankmassi@gmail.com>
2023-12-09 08:17:52 -0600

    WIP: 2023 9 part 1 ruby

--- ruby/lib/year_2023/day_09.rb
+++ ruby/lib/year_2023/day_09.rb
@@ -3,14 +3,24 @@
 module Year2023
   class Day09 < AdventOfCode::PuzzleBase
     def part1
+      @rows.collect { |row| find_next_diff(row) }.sum
     end
 
-    def part2
+    def part2; end
+
+    def find_next_diff(row)
+      diffs = (row.length - 1).times.collect { |n| row[n + 1] - row[n] }
+      diff = if diffs.uniq.length == 1
+               diffs.first
+             else
+               find_next_diff(diffs)
+             end
+      row.last + diff
     end
 
     # setup gets called as part of initialize
     def setup
-      @input # input is available as the raw input string
+      @rows = @input.split("\n").map { |row| row.split.map(&:to_i) }
     end
   end
 end
