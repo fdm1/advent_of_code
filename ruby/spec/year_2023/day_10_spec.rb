b71160f5f2fd520c757b944c876cf54ba9718384
Frank Massi <frankmassi@gmail.com>
2023-12-10 11:14:11 -0600

    WIP: 2023 10 part 2 setup

--- ruby/spec/year_2023/day_10_spec.rb
+++ ruby/spec/year_2023/day_10_spec.rb
@@ -13,9 +13,17 @@ RSpec.describe Year2023::Day10 do
     end
   end
 
-  xdescribe 'part2' do
+  describe 'part2' do
     it 'solves part2' do
-      expect(puzzle_test_runner(year: 2023, day: 10).part2).to eq(2)
+      expect(puzzle_test_runner(year: 2023, day: 10, suffix: 'part2_short').part2).to eq(4)
+    end
+
+    it 'solves part2 - longer' do
+      expect(puzzle_test_runner(year: 2023, day: 10, suffix: 'part2_longer').part2).to eq(8)
+    end
+
+    it 'solves part2 - longest' do
+      expect(puzzle_test_runner(year: 2023, day: 10, suffix: 'part2_longest').part2).to eq(10)
     end
   end
 end
