206c16ed13d601dd7550d88e632a1f8b8477c5e4
Frank Massi <frankmassi@gmail.com>
2023-12-11 07:48:05 -0600

    WIP: 2023 11 ruby setup

---
+++ ruby/lib/year_2023/day_11.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Year2023
+  class Day11 < AdventOfCode::PuzzleBase
+    def part1
+    end
+
+    def part2
+    end
+
+    # setup gets called as part of initialize
+    def setup
+      @input # input is available as the raw input string
+    end
+  end
+end
