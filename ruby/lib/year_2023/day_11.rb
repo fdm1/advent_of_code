d24951477fac7fbccdebe677f6a93cb85f31e403
Frank Massi <frankmassi@gmail.com>
2023-12-11 09:08:26 -0600

    WIP - expansion is too slow
    Need to rework math, maybe without actually expanding

--- ruby/lib/year_2023/day_11.rb
+++ ruby/lib/year_2023/day_11.rb
@@ -3,10 +3,25 @@
 module Year2023
   class Day11 < AdventOfCode::PuzzleBase
     def part1
+      expand_grid
+      find_galaxies
       find_total_distance
     end
 
-    def part2; end
+    def part2
+      expand_grid(@override_args[:size] || 1_000_000)
+
+      puts "grid expanded"
+      if @override_args[:size] == 10
+        puts
+        @grid.each { |row| puts row.join }
+
+      end
+
+      find_galaxies
+      puts "galaxies found"
+      find_total_distance
+    end
 
     def find_total_distance
       total_distance = 0
@@ -25,21 +40,20 @@ module Year2023
       (g1[0] - g2[0]).abs + (g1[1] - g2[1]).abs
     end
 
-    def expand_grid
+    def expand_grid(size = 1)
       # wide
       empty_columns = []
       @grid.first.each_with_index do |_cell, x|
         empty_columns << x if @grid.all? { |row| row[x] == '.' }
       end
-      empty_columns.reverse.each { |x| @grid.each { |row| row.insert(x, '.') } }
-
-      empty_rows = []
+      empty_columns.reverse.each { |x| @grid.each { |row| size.times { row.insert(x, '.') } } }
 
       # tall
+      empty_rows = []
       @grid.each_with_index do |row, y|
         empty_rows << y if row.all? { |cell| cell == '.' }
       end
-      empty_rows.reverse.each { |y| @grid.insert(y, Array.new(@grid.first.length, '.')) }
+      empty_rows.reverse.each { |y| size.times { @grid.insert(y, Array.new(@grid.first.length, '.')) } }
     end
 
     def find_galaxies
@@ -58,8 +72,6 @@ module Year2023
     # setup gets called as part of initialize
     def setup
       @grid = @input.split("\n").map(&:chars)
-      expand_grid
-      find_galaxies
     end
   end
 end
