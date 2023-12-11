5d550a8407b4af3787c18d7d9e1f517aabbb4426
Frank Massi <frankmassi@gmail.com>
2023-12-11 13:08:32 -0600

    WIP: closer - squeezing between pipes still not there
    Also slow (almost a minute)
    588 - too high

--- ruby/lib/year_2023/day_10.rb
+++ ruby/lib/year_2023/day_10.rb
@@ -71,7 +71,7 @@ module Year2023
       while position1.position != @start
         position1.move(@grid)
       end
-      @path = position1.visited
+      count_internal_points(position1.visited)
     end
 
     def set_initial_positions
@@ -92,12 +92,66 @@ module Year2023
       @positions << Position.new(@start[0], @start[1], :right, @grid) if grid_right && ['-', '7', 'J'].include?(grid_right)
     end
 
-    # setup gets called as part of initialize
+    def expand_grid
+      @grid.each do |row|
+        row.unshift('X') && row.push('X')
+      end
+      @grid.unshift(Array.new(@grid[0].length) { 'X' })
+      @grid.push(Array.new(@grid[0].length) { 'X' })
+    end
+
+    def next_adjacent_points(point, path)
+      res = []
+      res << [point[0] - 1, point[1]] if point[0].positive? && @grid[point[0] - 1][point[1]]
+      res << [point[0] + 1, point[1]] if @grid[point[0] + 1] && @grid[point[0] + 1][point[1]]
+      res << [point[0], point[1] - 1] if point[1].positive? && @grid[point[0]][point[1] - 1]
+      res << [point[0], point[1] + 1] if @grid[point[0]][point[1] + 1]
+      res.filter {|p| !path.include?(p) }
+    end
+
+    def count_internal_points(path)
+      open_points = Set.new(@edge_points)
+      closed_points = Set.new
+      @grid.each_with_index do |row, y|
+        row.each_with_index do |column, x|
+          current_point = [y, x]
+          next if open_points.include?(current_point)
+          next if path.include?(current_point)
+          adjacent_points = next_adjacent_points(current_point, path).filter {|p| !closed_points.include?(p) } + [current_point]
+          adjacent_points.map do |p|
+            points_checked = Set.new
+
+            can_get_out = AdventOfCode::Algorithms::BFS.new(
+              endpoint: current_point,
+              starting_points: open_points,
+            ).run_search! do |point|
+              points_checked << point
+              if closed_points.include?(point)
+                []
+              else
+                open_points.include?(point) ? open_points : next_adjacent_points(point, path)
+              end
+            end
+            if can_get_out
+              open_points.merge(points_checked)
+            else
+              closed_points.merge(points_checked)
+            end
+          end
+        end
+      end
+      closed_points.length
+    end
+
     def setup
       @grid = @input.split("\n").map(&:chars)
+      expand_grid # to give exit points for checking for internal or external
+      @edge_points = []
       @grid.each_with_index do |row, y|
-        x = row.index('S')
-        @start = [y, x] if x
+        start = row.index('S')
+        edge = row.index('X')
+        @start = [y, start] if start
+        @edge_points << [y, edge] if edge
       end
       set_initial_positions
     end
