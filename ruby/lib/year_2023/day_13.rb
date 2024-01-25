abed20b9a933702381e6c196800fcfa8fe94bc59
Frank Massi <frankmassi@gmail.com>
2024-01-25 10:03:54 -0600

    WIP

--- ruby/lib/year_2023/day_13.rb
+++ ruby/lib/year_2023/day_13.rb
@@ -3,14 +3,28 @@
 module Year2023
   class Day13 < AdventOfCode::PuzzleBase
     def part1
+      binding.pry
     end
 
     def part2
     end
 
+    def find_possible_reflections
+      @patterns.each do |pattern|
+        pattern[:row_matches] = pattern[:rows].collect.with_index {|r, i| r == pattern[:rows][i+1]}
+        pattern[:column_matches] = pattern[:columns].collect.with_index {|c, i| c == pattern[:columns][i+1]}
+      end
+    end
+
     # setup gets called as part of initialize
     def setup
-      @input # input is available as the raw input string
+      @patterns = @input.split("\n\n").collect do |p|
+        {
+          rows: p.split("\n"),
+          columns: p.split("\n").map {|r| r.split("")}.transpose.map {|c| c.join("")},
+        }
+      end
+      find_possible_reflections
     end
   end
 end
