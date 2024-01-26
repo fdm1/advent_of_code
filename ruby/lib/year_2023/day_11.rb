76bd1a4cbdd36caad7727604cc7a005449d3606c
Frank Massi <frankmassi@gmail.com>
2024-01-25 19:17:38 -0600

    WIP

--- ruby/lib/year_2023/day_11.rb
+++ ruby/lib/year_2023/day_11.rb
@@ -3,14 +3,19 @@
 module Year2023
   class Day11 < AdventOfCode::PuzzleBase
     def part1
+      binding.pry
     end
 
     def part2
     end
 
+    def expand_grid
+    end
+
     # setup gets called as part of initialize
     def setup
-      @input # input is available as the raw input string
+      @grid = @input.split("\n").map(&:chars)
+      expand_grid
     end
   end
 end
