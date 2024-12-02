fba89170b25824a6637d643f0c706a0aaa5777c8
Frank Massi <fdm1@users.noreply.github.com>
2024-12-02 10:44:03 -0600

    Brute force 2024-2-2 ruby (#32)

--- ruby/lib/year_2024/day_02.rb
+++ ruby/lib/year_2024/day_02.rb
@@ -3,11 +3,22 @@
 module Year2024
   class Day02 < AdventOfCode::PuzzleBase
     def part1
-      @row_arrays.filter { |r| row_is_safe?(r, false) }.count
+      @row_arrays.filter { |r| row_is_safe?(r) }.count
     end
 
     def part2
-      @row_arrays.filter { |r| row_is_safe?(r, true) }.count
+      @row_arrays.filter do |r|
+        if row_is_safe?(r)
+          true
+        else
+          # brute forcing out of laziness
+          r.count.times.collect do |i|
+            dup = r.dup
+            dup.delete_at(i)
+            row_is_safe?(dup)
+          end.any?
+        end
+      end.count
     end
 
     # setup gets called as part of initialize
@@ -17,37 +28,22 @@ module Year2024
 
     private
 
-    def row_is_safe?(row_array, allow_bad_level)
+    def row_is_safe?(row_array)
+      # handle issues with duplicate values
+      return false if row_array.uniq.length != row_array.length
+
       direction = nil
 
-      # handle issues with duplicate values
-      uniq_row = row_array.uniq
-      if uniq_row.length != row_array.length && !allow_bad_level
-        return false
-      elsif uniq_row.length == row_array.length - 1 && allow_bad_level
-        return row_is_safe?(uniq_row, false)
-      end
+      row_array.map.with_index do |a, i|
+        next if i.zero?
 
-      row_array.collect.with_index do |a, i|
         b = row_array[i - 1]
-
-        next if i.zero? # can't do anything based on first number alone
 
         if i == 1
           direction = a > b ? 1 : -1
         end
 
-        unless correct_direction?(a, b, direction) && safe_distance?(a, b)
-          return false unless allow_bad_level
-
-          dup1 = row_array.dup
-          dup1.delete_at(i)
-          return true if row_is_safe?(dup1, false)
-
-          dup2 = row_array.dup
-          dup2.delete_at(i - 1)
-          return row_is_safe?(dup2, false)
-        end
+        return false unless correct_direction?(a, b, direction) && safe_distance?(a, b)
       end
 
       true
@@ -60,6 +56,8 @@ module Year2024
     end
 
     def safe_distance?(x, y)
+      return true unless x && y
+
       distance = (x - y).abs
       distance >= 1 && distance <= 3
     end
